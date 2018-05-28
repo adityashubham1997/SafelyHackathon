53c37a24d579f1e599107ab62e6901fd092a7ecd
AyushKshitij45 <iayush45@gmail.com>
2018-05-28 16:50:28 +0530

    vbnh

--- app/src/main/java/com/example/admin/myapplication/HealthActivity.java
+++ app/src/main/java/com/example/admin/myapplication/HealthActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +13,15 @@ import java.util.ArrayList;
 
 public class HealthActivity extends AppCompatActivity {
     private TextView v;
+    String name;
+    String email;
+    String blood;
+    String gender;
+    String diab;
+    String b_p;
+    String cronic;
+    String number1;
+    public static  final String DEFAULT ="N/A";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +30,14 @@ public class HealthActivity extends AppCompatActivity {
         final ArrayList<com.example.admin.myapplication.List> disease = new ArrayList<com.example.admin.myapplication.List>();
 
 
-String name=getIntent().getStringExtra("name");
-        disease.add(new List("Name", R.drawable.blood_pressure,""+name));
-        disease.add(new List("Emergency contact", R.drawable.contact,getIntent().getStringExtra("number")));
-        disease.add(new List("Blood Group", R.drawable.blood_group,getIntent().getStringExtra("blood")));
-        disease.add(new List("Email", R.drawable.heart_rate,getIntent().getStringExtra("email")));
-        disease.add(new List("BP Status", R.drawable.blood_pressure,getIntent().getStringExtra("blood")));
-        disease.add(new List("Diabetes", R.drawable.diabetes_monitor,getIntent().getStringExtra("diab")));
-        disease.add(new List("Cronic diseases", R.drawable.heart_rate,getIntent().getStringExtra("cronic")));
+getData();
+        disease.add(new List("Name", R.drawable.blood_pressure,name));
+        disease.add(new List("Emergency contact", R.drawable.contact,number1));
+        disease.add(new List("Blood Group", R.drawable.blood_group,blood));
+        disease.add(new List("Email", R.drawable.heart_rate,email));
+        disease.add(new List("BP Status", R.drawable.blood_pressure,b_p));
+        disease.add(new List("Diabetes", R.drawable.diabetes_monitor,diab));
+        disease.add(new List("Cronic diseases", R.drawable.heart_rate,cronic));
 
         final ListAdapter listAdapter = new ListAdapter(this, disease);
 
@@ -44,4 +55,18 @@ String name=getIntent().getStringExtra("name");
     });
 }
 
+    private void getData()
+    {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        name = sharedPreferences.getString("name",DEFAULT);
+        cronic = sharedPreferences.getString("chronic",DEFAULT);
+        diab = sharedPreferences.getString("diab",DEFAULT);
+        b_p= sharedPreferences.getString("bp",DEFAULT);
+        blood = sharedPreferences.getString("blood",DEFAULT);
+        email = sharedPreferences.getString("email",DEFAULT);
+        gender = sharedPreferences.getString("sex",DEFAULT);
+        number1 = sharedPreferences.getString("emergencycontactnumber",DEFAULT);
+
+    }
 }
